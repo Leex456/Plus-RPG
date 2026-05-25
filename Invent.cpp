b15eb837140c1d56c5094171fa7b50ab931167ce
babaam224 <164300609+babaam224@users.noreply.github.com>
2026-05-25 14:54:27 +0200

    simplified inventory system

--- Invent.cpp
+++ Invent.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
 using namespace std;
 
-// Node structure
+// Node
 struct Item {
     string name;
     Item* next;
 };
 
-// Inventory class
 class Inventory {
+
 private:
     Item* head;
 
 public:
+
     // Constructor
     Inventory() {
         head = NULL;
     }
 
-    // Add item to inventory
+    // Add item
     void addItem(string itemName) {
-        Item* newItem = new Item();
+
+        Item* newItem = new Item;
 
         newItem->name = itemName;
         newItem->next = NULL;
 
-        // If inventory empty
+        // First item
         if (head == NULL) {
             head = newItem;
         }
         else {
-            // Go to last node
+
             Item* temp = head;
 
             while (temp->next != NULL) {
@@ -40,63 +42,70 @@ public:
             temp->next = newItem;
         }
 
-        cout << itemName << " added to inventory.\n";
+        cout << itemName << " added!\n";
     }
 
-    // Display inventory
+    // Show inventory
     void showInventory() {
+
         if (head == NULL) {
-            cout << "Inventory is empty.\n";
+            cout << "Inventory empty.\n";
             return;
         }
 
         Item* temp = head;
 
-        cout << "\n=== INVENTORY ===\n";
-
-        int index = 1;
+        cout << "\nInventory:\n";
 
         while (temp != NULL) {
-            cout << index << ". " << temp->name << endl;
+
+            cout << "- " << temp->name << endl;
+
             temp = temp->next;
-            index++;
         }
     }
 
     // Remove item
     void removeItem(string itemName) {
+
+        // Empty inventory
         if (head == NULL) {
-            cout << "Inventory is empty.\n";
+            cout << "Inventory empty.\n";
             return;
         }
 
         // Remove first item
         if (head->name == itemName) {
-            Item* deleteNode = head;
+
+            Item* temp = head;
+
             head = head->next;
 
-            delete deleteNode;
+            delete temp;
 
-            cout << itemName << " removed.\n";
+            cout << itemName << " removed!\n";
+
             return;
         }
 
-        Item* temp = head;
+        Item* current = head;
 
-        while (temp->next != NULL &&
-               temp->next->name != itemName) {
-            temp = temp->next;
+        while (current->next != NULL &&
+               current->next->name != itemName) {
+
+            current = current->next;
         }
 
-        // Item found
-        if (temp->next != NULL) {
-            Item* deleteNode = temp->next;
+        // Found item
+        if (current->next != NULL) {
 
-            temp->next = temp->next->next;
+            Item* temp = current->next;
 
-            delete deleteNode;
+            current->next = current->next->next;
 
-            cout << itemName << " removed.\n";
+            delete temp;
+
+            cout << itemName << " removed!\n";
         }
         else {
             cout << "Item not found.\n";
