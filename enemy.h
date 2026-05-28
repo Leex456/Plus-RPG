bf0115bfeaf8f7d404bc67de8b24af6cc70d0b40
babaam224 <164300609+babaam224@users.noreply.github.com>
2026-05-28 17:38:46 +0800

    added more enemy

--- enemy.h
+++ enemy.h
@@ -59,5 +59,103 @@ inline void generateNextIntent() {
             if (monsterHP > monsterMaxHP) monsterHP = monsterMaxHP;
         }
     }
+
+    if (monsterName == "Bat") {
+        if (r == 0) {   
+            enemyIntent = "Attack (" + to_string(1 + enemy_strength) + " DMG)";
+            enemy_dmg = 2;
+        }
+        else if (r == 1) {
+            enemyIntent = "Drain 1 (deal 1 DMG and heal self for 1 HP)";  
+            enemy_regeneration += 1;
+            enemy_dmg = 1; 
+        }
+        else {
+            enemyIntent = "Drain 3 (deal 3 DMG and heal self for 3 HP)";
+            enemy_dmg = 3;
+            monsterHP += 3;
+            if (monsterHP > monsterMaxHP) monsterHP = monsterMaxHP;
+        }
+    }
+
+    if (monsterName == "Frog") {
+        if (r == 0) {   
+            enemyIntent = "Attack (" + to_string(1 + enemy_strength) + " DMG)";
+            enemy_dmg = 2;
+        }
+        else if (r == 1) {
+            enemyIntent = "Poison (deal 1 DMG and apply 1 Poison)";  
+            enemy_poison += 1;
+            enemy_dmg = 1; 
+        }
+        else {
+            enemyIntent = "Poison 3 (deal 3 DMG and apply 3 Poison)";  
+            enemy_poison += 3;
+            enemy_dmg = 3; 
+        }
+    }
+    
+    if (monsterName == "Slime") {
+        if (r == 0) {   
+            enemyIntent = "Attack (" + to_string(1 + enemy_strength) + " DMG)";
+            enemy_dmg = 3;
+        }
+        else if (r == 1) {
+            enemyIntent = "Poison 2 (deal 2 DMG and apply 2 Poison)";  
+            enemy_poison += 2;
+            enemy_dmg = 2; 
+        }
+        else {
+            enemyIntent = "Poison 5 (deal 5 DMG and apply 5 Poison)";  
+            enemy_poison += 5;
+            enemy_dmg = 5; 
+        }
+    }
+
+    if (monsterName == "Porcupine") {
+        if (r == 0) {   
+            enemyIntent = "Attack (" + to_string(1 + enemy_strength) + " DMG)";
+            enemy_dmg = 1;
+        }
+        else if (r == 1) {
+            enemyIntent = "Thorns 1 (apply 1 Thorns)";  
+            enemy_thorns += 1;
+            enemy_dmg = 0; 
+        }
+        else {
+            enemyIntent = "Thorns 3 (apply 3 Thorns)";  
+            enemy_thorns += 3;
+            enemy_dmg = 0; 
+        }
+    }
+
+    if (monsterName == "Gambler") {
+        if (r == 0) {   
+            enemyIntent = "Unknown";
+            enemy_dmg = rand() % 3;
+        }
+        else if (r == 1) {
+            enemyIntent = "Unknown";  
+            enemy_strength += rand() % 3;
+            enemy_dmg = 0; 
+        }
+        else if (r == 2) {
+            enemyIntent = "Unknown";  
+            enemy_regeneration += rand() % 3;
+            enemy_dmg = 0; 
+        }
+        else if (r == 3) {
+            enemyIntent = "Unknown";  
+            player_regeneration += rand() % 3;
+            enemy_dmg = 0; 
+        }
+        else {
+            enemyIntent = "Unknown";  
+            player_strength += rand() % 3;
+            enemy_dmg = 0; 
+        }
+    }
+
+    
 }
 #endif
